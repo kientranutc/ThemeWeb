5bad36a27e9d05a814b5198f92dd98772b9acde5
kientc6811 <kientc6811@co-well.com.vn>
2017-12-14 15:19:04 +0700

    home and order

---
+++ Sim/assets/js/main_carousel.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $(".owl-carousel").owlCarousel({
+        items: 2,
+        loop: true,
+        margin: 5,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        autoplayHoverPause: true
+    });
+});
